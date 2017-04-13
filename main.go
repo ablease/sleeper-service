a5b09e1ee4ddefb7f33804339035ca93e77dc89a
Alex Blease <ablease@pivotal.io>
2017-04-13 17:35:27 +0100

    Sleeper Service has become eccentric

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/ablease/sleeper-service/sleeperservice"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	sleeperservice.AttachRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8888", nil))
+}
